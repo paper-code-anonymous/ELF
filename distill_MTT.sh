6da1e43ffc06e3ed1fef4664835d886821ae7122
paper-code-anonymous <133186056+paper-code-anonymous@users.noreply.github.com>
2023-05-16 22:35:04 +0800

    Update and rename distill_mtt.sh to distill_MTT.sh

--- distill_MTT.sh
+++ distill_MTT.sh
@@ -1,4 +1,4 @@
-python -u distill_mtt.py --CUDA_VISIBLE_DEVICES=0,1,2,3 \
+python -u distill_MTT.py --CUDA_VISIBLE_DEVICES=0,1,2,3 \
 --model=ConvNet --dataset=CIFAR100 --ipc=10 \
 --syn_steps=20 --expert_epochs=2 --max_start_epoch=40 \
 --lr_img=1e3 --lr_lr=1e-5 --lr_teacher=1e-2 \
